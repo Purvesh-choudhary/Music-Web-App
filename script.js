2818675c2ac3cc35701ae11f64b730fd994e7d7b
Purveh Choudhary <purveshchoudhary99@gmail.com>
2024-02-23 21:03:41 +0530

    Home Page Done

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ console.log("i");
 
 function startIntro() {
     function callstartintro() {
-        
-        startPop.style.display = "none";
+    startPop.style.display = "none";
     }
-    setTimeout(callstartintro, 4000);
+    setTimeout(callstartintro, 5000);
 
 }
 window.onload = function () {
+    startPop.style.opacity = '0';
     startIntro();
 }
