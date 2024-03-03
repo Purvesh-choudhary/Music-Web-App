666f0ecb9c7742442600cc02e9808df0391bd5d9
Purveh Choudhary <98455640+Purvesh-choudhary@users.noreply.github.com>
2024-03-03 23:16:36 +0530

    added audio Playback

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const startPop = document.getElementById('startPop');
+const MusicCatalog = document.getElementById('musicCatalog'); 
+
+let currentAudioElement;
+
 console.log("i");
 
 function startIntro() {
@@ -8,6 +12,60 @@ function startIntro() {
     setTimeout(callstartintro, 5000);
 
 }
+
+MusicCatalog.addEventListener("click", async function (e){
+    const target = e.target;
+
+    if (target.tagName === "IMG") {
+        console.log("IMG IS CLICKED" + target.getAttribute("src"));
+        const parentDiv = target.parentElement;
+        const sourceElement = parentDiv.querySelector("source");
+        const audioSrc = sourceElement.getAttribute("src");
+        console.log("AUDIO SRC IS HERE - " + audioSrc); 
+        playingSong(audioSrc);
+
+        if (currentAudioElement && !currentAudioElement.paused && currentAudioElement.src === audioSrc) {
+            // If the clicked audio is already playing, pause it
+            currentAudioElement.pause();
+        } else {
+            if (currentAudioElement && !currentAudioElement.paused) {
+                // If another audio is already playing, stop it
+                await stopPlayingSong();
+            }
+            // Play the clicked audio
+            await playingSong(audioSrc);
+        }
+    }
+});
+
+function playingSong(songsrc){
+    return new Promise((resolve, reject) => {
+        currentAudioElement = new Audio(songsrc);
+        currentAudioElement.play()
+            .then(() => {
+                resolve();
+            })
+            .catch((error) => {
+                console.error("Error playing audio:", error);
+                reject(error);
+            });
+    });
+}
+
+function stopPlayingSong() {
+    return new Promise((resolve, reject) => {
+        if (currentAudioElement && !currentAudioElement.paused) {
+            currentAudioElement.pause(); // Pause the currently playing song
+            currentAudioElement.currentTime = 0; // Reset playback to the beginning
+            resolve();
+        } else {
+            resolve(); // If no song is playing, resolve immediately
+        }
+    });
+}
+
+
+
 window.onload = function () {
     startPop.style.opacity = '0';
     startIntro();
